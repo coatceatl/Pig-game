22000e889a0495bca4931f03fd45e374186e6dcb
Oksana Tkachenko <oksana@crystax.net>
2018-04-03 00:33:17 +0300

    fixed bugfixed bug with class active

--- index.js
+++ index.js
@@ -60,6 +60,11 @@ document.querySelector('.btn-new').addEventListener('click', function() {
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
   document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
 });
 
 
