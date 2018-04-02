861ca5687e80ab6d208a1232618c789abb4a7e69
Oksana Tkachenko <oksana@crystax.net>
2018-04-03 01:30:15 +0300

    fixed bug (adding score after winning)

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  const dice = Math.floor(Math.random() * 6 ) + 1;
+  if(gamePlaying) {
+    const dice = Math.floor(Math.random() * 6 ) + 1;
 
-  const diceDisplay = document.querySelector('.dice');
-  diceDisplay.style.display = 'block';
-  diceDisplay.src = 'img/dice-' + dice + '.png';
+    const diceDisplay = document.querySelector('.dice');
+    diceDisplay.style.display = 'block';
+    diceDisplay.src = 'img/dice-' + dice + '.png';
 
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  scores[activePlayer] += roundScore;
-  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+  if(gamePlaying) {
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-  if (scores[activePlayer] >= 20) {
-    document.getElementById('name-' + activePlayer).textContent = 'Winner!!!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    nextPlayer();
+    if (scores[activePlayer] >= 100) {
+      document.getElementById('name-' + activePlayer).textContent = 'Winner!!!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
@@ -50,6 +55,7 @@ function init() {
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0;
+  gamePlaying = true;
 
   document.querySelector('.dice').style.display = 'none';
 
