c5ca7cec0519d62e6679c7b1def0eb9c5c00a9b0
Oksana Tkachenko <oksana@crystax.net>
2018-04-02 13:28:08 +0300

    adds fontawesome and changed css

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-const dice = Math.floor(Math.random() * 6 ) + 1;
+/*
+document.getElementByClassName('btn-roll').addEventListener('click', function() {
+  const dice = Math.floor(Math.random() * 6 ) + 1;
+
+  const diceDisplay = document.getElementByClassName('dice');
+  diceDisplay.src = 'dice-' + dece + '.png';
+});
+*/
 
 //document.getElementById('current-0').textContent = dice;
+
