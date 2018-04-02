c6998df3413a858e7a079326693044302a2131a7
Oksana Tkachenko <oksana@crystax.net>
2018-04-02 19:22:12 +0300

    changed displai .dice for start and for game

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
+document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   const dice = Math.floor(Math.random() * 6 ) + 1;
 
   const diceDisplay = document.querySelector('.dice');
+  diceDisplay.style.display = 'block';
   diceDisplay.src = 'img/dice-' + dice + '.png';
 
   if (dice !== 1) {
     roundScore += dice;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
   } else {
     console.log(dice);
   }
