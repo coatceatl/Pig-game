05d41373e317ec9839e0d0eadfd6409050ec3434
Oksana Tkachenko <oksana@crystax.net>
2018-04-02 19:31:20 +0300

    adds score to current score and current player

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const scores = [0,0];
-const roundScore = 0;
-const activePlayer = 0;
+let scores = [0,0];
+let roundScore = 0;
+let activePlayer = 0;
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -16,7 +16,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDisplay.src = 'img/dice-' + dice + '.png';
 
   if (dice !== 1) {
-    roundScore += dice;
+    roundScore = dice;
+    console.log(roundScore);
     document.getElementById('current-' + activePlayer).textContent = roundScore;
   } else {
     console.log(dice);
