3f70a56d5f37e7fea9436b54c32530882fa79855
Oksana Tkachenko <oksana@crystax.net>
2018-04-02 15:09:45 +0300

    displaied the random dice result

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const scores = [0,0];
+const roundScore = 0;
+const activePlayer = 0;
+
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-/*
-document.getElementByClassName('btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-roll').addEventListener('click', function() {
   const dice = Math.floor(Math.random() * 6 ) + 1;
 
-  const diceDisplay = document.getElementByClassName('dice');
-  diceDisplay.src = 'dice-' + dece + '.png';
+  const diceDisplay = document.querySelector('.dice');
+  diceDisplay.src = 'img/dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScore += dice;
+  } else {
+    console.log(dice);
+  }
 });
-*/
 
 //document.getElementById('current-0').textContent = dice;
 
