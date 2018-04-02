087ab2752760694e42fd396dc85b837b9c0ad99c
Oksana Tkachenko <oksana@crystax.net>
2018-04-03 00:26:22 +0300

    WIP, adds event listener for button new game

--- index.js
+++ index.js
@@ -50,3 +50,16 @@ function nextPlayer() {
   document.querySelector('.dice').style.display = 'none';
 }
 
+document.querySelector('.btn-new').addEventListener('click', function() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.dice').style.display = 'none';
+});
+
+
